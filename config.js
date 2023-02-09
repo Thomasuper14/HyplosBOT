78ce2519eb61047f83937f0b2ada97c73d5cd591
Thomasuper14 <75450804+Thomasuper14@users.noreply.github.com>
2023-02-09 18:23:38 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.autoReadAll = false
 global.antitags = true
 
 //aumto functioner
-//global.autoTyping = false //auto tying in gc (true to on, false to off)
+//global.autoTyping = false //auto tying in gc (true to yon, false to off)
 //global.autoRecord = true //auto recording (true to on, false to off)
 
 global.Owner = ['393477037227','393662531766']  //If you want singal number so global.Owner = ['916297175943'] Change into your number
