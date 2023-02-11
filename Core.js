155d1db283f26d7257ec2bf7a1e791bc8749fb4b
plo19 <124474896+plo19@users.noreply.github.com>
2023-02-11 11:25:37 +0100

    Update Core.js

--- Core.js
+++ Core.js
@@ -54,12 +54,6 @@ const { mediafireDl } = require('./lib/mediafire.js')
 const kaitime = moment.tz('Asia/Kolkata').format('HH:mm:ss')
 const kaidate = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
 const time2 = moment().tz('Asia/Kolkata').format('HH:mm:ss')
-const cron = require('node-cron');
-const messages = [
-  "Questo è il primo messaggio programmato.",
-  "Questo è il secondo messaggio programmato.",
-  "Questo è il terzo messaggio programmato."
-];
 
         if(time2 < "23:59:00"){
 
@@ -1599,10 +1593,6 @@ if (smallinput=='ping') {
       reply (`Mention not ${pushname} 😇. I am a bot afterall.`);
     }
 	
-cron.schedule("*/10 * * * * *", function() {
-  	const message = messages[Math.floor(Math.random() * messages.length)];
-  	console.log(message);
-	});
 	
 	
 	
